c810a76ad788a1bb1b4ea7d10ee2e08c90f2fcdb
DennRoy <uxcur@student.kit.edu>
2017-03-01 14:13:51 +0100

    Updated script

--- CombineTools/scripts/plotAsympOnLHC.py
+++ CombineTools/scripts/plotAsympOnLHC.py
@@ -7,6 +7,16 @@ import CombineHarvester.CombineTools.plotting as plot
 import argparse
 import sys
 
+# This script may be used to plot a toy distribution over the asymptotic prediction. The only
+# required input is the path to the rootfile containing the toys (HypoTestResult class). This
+# root file is usually produced by the HybridNewGrid method. It is possible to input multiple
+# root files (without using an argument). See below (near line 50) for a list of all
+# optional parameters.
+
+# If no comparison with the asymptotic prediction is required, use at --no_asymp argument.
+# Otherwise, it is necessary to manually input the result of the asymptotic calculation into
+# this script (near line 70). The values q_mu and q_A are required.
+
 def DrawCMSLogo(pad, cmsText, extraText):
     pad.cd()
     cmsTextSize=0.8
@@ -43,31 +53,31 @@ ROOT.gROOT.SetBatch(ROOT.kTRUE)
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('files', help='Input root files containing HypoTestResult objects (no option prefix needed)', nargs='*')
 parser.add_argument('--name', '-n', help='Appendix on the output name', default="")
-parser.add_argument('--alt', help='Name for the alternative hypothesis', default="S+B")
-parser.add_argument('--null', help='Name for the null hypothesis', default="B")
+parser.add_argument('--alt', help='Name for the alternative hypothesis', default="H_{1}")
+parser.add_argument('--null', help='Name for the null hypothesis', default="H_{0}")
 parser.add_argument('--bin_number', '-bins', help='Set the number of bins', default=120)
 parser.add_argument('--expected', '-exp', help='Fix q_obs to a different value so CLb = 0.5', action='store_true')
 parser.add_argument('--logx', '-logx', help='Draw x-axis in log scale', action='store_true')
 parser.add_argument('--no_asymp', help='Show only toy distribution', action='store_true')
 parser.add_argument('--fit_qa', help='Fit qA to the toy distribution of the null hypothesis', action='store_true')
+parser.add_argument('--sb', help='Fit qA to the toy distribution of the alternative hypothesis instead', action='store_true')
 parser.add_argument('--diff', help='Make a plot of the difference between Asymptotic and LHC toy based test statistic', action='store_true')
 parser.add_argument('--display', '-d', help='Display plots right when they are done', action='store_true')
 args = parser.parse_args()
 
 
 AsympOnLHC = not args.no_asymp
+args.bin_number = int(args.bin_number)
 if AsympOnLHC:
   # Insert output from combine here #<----------------------------------------------
-  # These 5 values need to be taken from where it says 'At x = 1.000000:'
-  qmu = 0.78743
-  qA = 0.63609
-  clsb = 0.18608
-  clb = 0.46221
-  cls = 0.40259
+  # These values need to be taken from where it says 'At x = 1.000000:'
+  qmu = 3.24655
+  qA = 2.51344
 
 
   # TODO: Somehow write numbers to json, so they can be read more easily?
 
+expclb=0.5 #0.025,0.16,0.5,0.84,0.975
 results = []
 for file in args.files:
   found_res = False
@@ -127,7 +137,7 @@ val_obs = result.GetTestStatisticData() * 2.
 
 if args.expected:
   null_vals.sort()
-  val_obs = null_vals[int(min(floor(0.5 * len(null_vals) +0.5), len(null_vals)-1))]#null_vals[len(null_vals)/2]
+  val_obs = null_vals[int(min(floor((1-expclb) * len(null_vals) +0.5), len(null_vals)-1))]#null_vals[len(null_vals)/2]
   result.SetTestStatisticData(val_obs/2)
 
 if len(null_vals) == 0 or len(alt_vals) == 0:
@@ -149,9 +159,6 @@ max_plot_range = max_val + 0.05 * (max_val - min_val)
 if AsympOnLHC:
   if args.expected:
     eN = ROOT.Math.normal_quantile(0.5, 1.0)
-    clb = 0.5
-    clsb = ROOT.Math.normal_cdf_c(math.sqrt(qA)-eN, 1.)
-    cls = clsb * 2
     qmu = qA
 
   TotalToysSB=result.GetAltDistribution().GetSize()
@@ -176,7 +183,7 @@ if AsympOnLHC:
     mult = max(mult1, mult2)
     if mult*qA > max_val: max_plot_range = mult*qA + 0.05 * (max_val - min_val)
     TotalToysSB=result.GetAltDistribution().GetSize()*(max_plot_range-min_plot_range)/args.bin_number
-    TotalToysB=result.GetNullDistribution().GetSize()*(max_plot_range-min_plot_range)/args.bin_number    
+    TotalToysB=result.GetNullDistribution().GetSize()*(max_plot_range-min_plot_range)/args.bin_number
 
 hist_null = ROOT.TH1F('null', 'null', args.bin_number, min_plot_range, max_plot_range)
 hist_alt = ROOT.TH1F('alt', 'alt', args.bin_number, min_plot_range, max_plot_range)
@@ -184,8 +191,9 @@ for val in null_vals: hist_null.Fill(val)
 for val in alt_vals: hist_alt.Fill(val)
 hist_alt.SetLineColor(ROOT.TColor.GetColor(4, 4, 255))
 hist_alt.SetFillColor(plot.CreateTransparentColor(ROOT.TColor.GetColor(4, 4, 255), 0.4))
-hist_alt.GetXaxis().SetTitle('-2 #times ln(^{}L_{%s}/^{}L_{%s})' % (alt_label, null_label))
-hist_alt.GetYaxis().SetTitle('Pseudo-experiments')
+hist_alt.GetXaxis().SetTitle('q')
+hist_alt.GetYaxis().SetTitle('Toys')
+hist_alt.GetYaxis().SetTitleOffset(1.0)
 hist_alt.SetMinimum(0.8)
 hist_alt.Draw()
 hist_null.SetLineColor(ROOT.TColor.GetColor(252, 86, 11))
@@ -197,7 +205,10 @@ obs.SetLineColor(ROOT.kRed)
 obs.SetLineWidth(3)
 obs.Draw()
 #plot.FixTopRange(pads[1], plot.GetPadYMax(pads[1]), 0.25)
-leg = ROOT.TLegend(0.76-ROOT.gPad.GetRightMargin(), 0.78-ROOT.gPad.GetTopMargin(), 0.98-ROOT.gPad.GetRightMargin(), 0.98-ROOT.gPad.GetTopMargin(), '', 'NBNDC')
+if AsympOnLHC:
+  leg = ROOT.TLegend(0.68-ROOT.gPad.GetRightMargin(), 0.72-ROOT.gPad.GetTopMargin(), 0.99-ROOT.gPad.GetRightMargin(), 0.99-ROOT.gPad.GetTopMargin(), '', 'NBNDC')
+else:
+  leg = ROOT.TLegend(0.68-ROOT.gPad.GetRightMargin(), 0.85-ROOT.gPad.GetTopMargin(), 0.99-ROOT.gPad.GetRightMargin(), 0.99-ROOT.gPad.GetTopMargin(), '', 'NBNDC')
 leg.AddEntry(hist_alt, alt_label, 'F')
 leg.AddEntry(hist_null, null_label, 'F')
 if args.expected:
@@ -206,50 +217,46 @@ else:
   leg.AddEntry(obs, 'Observed', 'L')
 pads[0].cd()
 if args.fit_qa:
-  pt_l1 = ROOT.TPaveText(0.33, 0.66, 0.43, 0.78, 'NDCNB')
+  pt_l1 = ROOT.TPaveText(0.23, 0.66, 0.33, 0.78, 'NDCNB')
 else:
-  pt_l1 = ROOT.TPaveText(0.33, 0.72, 0.43, 0.78, 'NDCNB')
+  pt_l1 = ROOT.TPaveText(0.23, 0.72, 0.33, 0.78, 'NDCNB')
 pt_l1.AddText('Model:')
 pt_l1.AddText('Toys:')
 if args.fit_qa:
-  pt_l1.AddText('Old qA:')
-  pt_l1.AddText('Fit qA:')
+  pt_l1.AddText('Old q_{A}:')
+  pt_l1.AddText('Fit q_{A}:')
 plot.Set(pt_l1, TextAlign=11, TextFont=62, BorderSize=0)
 pt_l1.Draw()
-pt_la = ROOT.TPaveText(0.20, 0.80, 0.30, 0.95, 'NDCNB')
+pt_la = ROOT.TPaveText(0.20, 0.80, 0.30, 0.99, 'NDCNB')
 pt_la.AddText('')
-pt_la.AddText('CLs+b:')
-pt_la.AddText('CLb:')
-pt_la.AddText('CLs:')
+pt_la.AddText('CL_{s+b} :')
+pt_la.AddText('CL_{b} :')
+pt_la.AddText('CL_{s} :')
 if args.expected and AsympOnLHC:
-  pt_la.AddText('q_exp:')
+  pt_la.AddText('q_{exp} :')
 else:
-  pt_la.AddText('q_obs:')
+  pt_la.AddText('q_{obs} :')
 plot.Set(pt_la, TextAlign=11, TextFont=62, BorderSize=0)
 pt_la.Draw()
 if AsympOnLHC:
-  pt_t1 = ROOT.TPaveText(0.35, 0.92, 0.54, 0.95, 'NDCNB')
-  pt_t2 = ROOT.TPaveText(0.55, 0.92, 0.74, 0.95, 'NDCNB')
-  pt_t3 = ROOT.TPaveText(0.75, 0.92, 0.94, 0.95, 'NDCNB')
+  pt_t1 = ROOT.TPaveText(0.33, 0.95, 0.52, 0.99, 'NDCNB')
+  pt_t2 = ROOT.TPaveText(0.55, 0.95, 0.74, 0.99, 'NDCNB')
   pt_t1.AddText('Toy based')
-  pt_t2.AddText('Asymptotic formula')
-  pt_t3.AddText('Asymptotic integral')
+  pt_t2.AddText('Asymptotic')
   plot.Set(pt_t1, TextAlign=11, TextFont=62, BorderSize=0)
   pt_t1.Draw()
   plot.Set(pt_t2, TextAlign=11, TextFont=62, BorderSize=0)
   pt_t2.Draw()
-  plot.Set(pt_t3, TextAlign=11, TextFont=62, BorderSize=0)
-  pt_t3.Draw()
 if args.fit_qa and AsympOnLHC:
-  pt_r1 = ROOT.TPaveText(0.41, 0.66, 0.71, 0.78, 'NDCNB')
+  pt_r1 = ROOT.TPaveText(0.32, 0.66, 0.62, 0.78, 'NDCNB')
 else:
-  pt_r1 = ROOT.TPaveText(0.41, 0.72, 0.71, 0.78, 'NDCNB')
+  pt_r1 = ROOT.TPaveText(0.32, 0.72, 0.62, 0.78, 'NDCNB')
 pt_r1.AddText('%s [%s = %s, %s = %s]' % ('m_{h}^{mod+}', 'mA', maval, 'tanb', tanbval))
 pt_r1.AddText('%i (%s) + %i (%s)' % (result.GetNullDistribution().GetSize(), null_label, result.GetAltDistribution().GetSize(), alt_label))
 if not args.fit_qa or not AsympOnLHC:
   plot.Set(pt_r1, TextAlign=11, TextFont=42, BorderSize=0)
   pt_r1.Draw()
-pt_b1 = ROOT.TPaveText(0.35, 0.80, 0.54, 0.92, 'NDCNB')
+pt_b1 = ROOT.TPaveText(0.33, 0.79, 0.52, 0.95, 'NDCNB')
 pt_b1.AddText('%.3f #pm %.3f' % (result.CLsplusb(), result.CLsplusbError()))
 pt_b1.AddText('%.3f #pm %.3f' % (result.CLb(), result.CLbError()))
 pt_b1.AddText('%.3f #pm %.3f' % (result.CLs(), result.CLsError()))
@@ -257,33 +264,29 @@ pt_b1.AddText('%.5f' % (val_obs))
 plot.Set(pt_b1, TextAlign=11, TextFont=42, BorderSize=0)
 pt_b1.Draw()
 if AsympOnLHC:
-  pt_b2 = ROOT.TPaveText(0.55, 0.83, 0.74, 0.92, 'NDCNB')
-  pt_b2.AddText('%.3f' % (clsb))
-  pt_b2.AddText('%.3f' % (clb))
-  pt_b2.AddText('%.3f' % (cls))
-  pt_b3 = ROOT.TPaveText(0.75, 0.83, 0.94, 0.92, 'NDCNB')
-  pt_b4 = ROOT.TPaveText(0.64, 0.80, 0.83, 0.83, 'NDCNB')
-  pt_b4.AddText('%.5f' % (qmu))
-  plot.Set(pt_b2, TextAlign=11, TextFont=42, BorderSize=0)
-  pt_b2.Draw()
-  plot.Set(pt_b4, TextAlign=11, TextFont=42, BorderSize=0)
-  pt_b4.Draw()
-DrawCMSLogo(pads[0], 'CMS', "(private work)")
+  pt_b2 = ROOT.TPaveText(0.55, 0.79, 0.74, 0.95, 'NDCNB')
+#DrawCMSLogo(pads[0], 'CMS', "(private work)")
 
 if AsympOnLHC:
-
-  f1 = ROOT.TF1("f1","(x<=[1])*[0]/(sqrt(8*TMath::Pi()*x))*(TMath::Exp(-1/2*x)) + (x>[1])*[0]/(sqrt(8*TMath::Pi()*[1]))*(TMath::Exp(-1/(8*[1])*(x+[1])*(x+[1])))",0,max_plot_range)
+  deltarange=0.00000000001#max_plot_range/args.bin_number
+  f1 = ROOT.TF1("f1","(x<=[2])*0.5*[0]+(x>[2])*(x<=[1])*[0]/(sqrt(8*TMath::Pi()*x))*(TMath::Exp(-1/2*x)) + (x>[1])*[0]/(sqrt(8*TMath::Pi()*[1]))*(TMath::Exp(-1/(8*[1])*(x+[1])*(x+[1])))",0,max_plot_range)
   f1.FixParameter(0,TotalToysSB)
   f1.SetParameter(1,qA)
+  f1.FixParameter(2,deltarange)
 
-  f2 = ROOT.TF1("f2","(x<=[1])*[0]/(sqrt(8*TMath::Pi()*x))*(TMath::Exp(-1/2*(sqrt(x)-sqrt([1]))*(sqrt(x)-sqrt([1])))) + (x>[1])*[0]/(sqrt(8*TMath::Pi()*[1]))*(TMath::Exp(-1/(8*[1])*(x-[1])*(x-[1])))",0,max_plot_range)
+  f2 = ROOT.TF1("f2","(x<=[2])*[3]*[0]+(x>[2])*(x<=[1])*[0]/(sqrt(8*TMath::Pi()*x))*(TMath::Exp(-1/2*(sqrt(x)-sqrt([1]))*(sqrt(x)-sqrt([1])))) + (x>[1])*[0]/(sqrt(8*TMath::Pi()*[1]))*(TMath::Exp(-1/(8*[1])*(x-[1])*(x-[1])))",0,max_plot_range)
 
   f2.FixParameter(0,TotalToysB)
   f2.SetParameter(1,qA)
+  f2.FixParameter(2,deltarange)
+  f2.FixParameter(3,ROOT.Math.normal_cdf(math.sqrt(qA),1))
 
   if args.fit_qa:
     pt_r1.AddText('%.5f' % (qA))
-    hist_null.Fit(f2, "QR0")
+    if args.sb:
+      hist_alt.Fit(f2, "QR0")#,"",max_plot_range/args.bin_number+0.01,max_plot_range)
+    else:
+      hist_null.Fit(f2, "QR0","",max_plot_range/args.bin_number+0.01,max_plot_range)
     qA = f2.GetParameter(1)
     f1.SetParameter(1,qA)
     pt_r1.AddText('%.5f' % (qA))
@@ -303,12 +306,15 @@ if AsympOnLHC:
   else:
     asy_sb=f1.Integral(qmu,max_plot_range)/TotalToysSB
     asy_b=f2.Integral(qmu,max_plot_range)/TotalToysB
+  #print "total sb: "+str(f1.Integral(0,max_plot_range)/TotalToysSB)
+  #print "total b: "+str(f2.Integral(0,max_plot_range)/TotalToysSB)
   pads[0].cd()
-  pt_b3.AddText('%.3f' % (asy_sb))
-  pt_b3.AddText('%.3f' % (asy_b))
-  pt_b3.AddText('%.3f' % (asy_sb/asy_b))
-  plot.Set(pt_b3, TextAlign=11, TextFont=42, BorderSize=0)
-  pt_b3.Draw()
+  pt_b2.AddText('%.3f' % (asy_sb))
+  pt_b2.AddText('%.3f' % (asy_b))
+  pt_b2.AddText('%.3f' % (asy_sb/asy_b))
+  pt_b2.AddText('%.5f' % (qmu))
+  plot.Set(pt_b2, TextAlign=11, TextFont=42, BorderSize=0)
+  pt_b2.Draw()
   pads[1].cd()
 
   hist_alt.SetMaximum(histmax*2)
@@ -321,9 +327,9 @@ if AsympOnLHC:
   leg.AddEntry(f1, alt_label+' Asymptotic', 'L')
   leg.AddEntry(f2, null_label+' Asymptotic', 'L')
   if args.expected:
-    leg.AddEntry(obs2, 'Asymp Expected', 'L')
+    leg.AddEntry(obs2, 'Asymp. Expected', 'L')
   else:
-    leg.AddEntry(obs2, 'Asymp Observed', 'L')
+    leg.AddEntry(obs2, 'Asymp. Observed', 'L')
 
   f1.SetLineWidth(3)
   f1.SetLineColor(ROOT.kBlue)
